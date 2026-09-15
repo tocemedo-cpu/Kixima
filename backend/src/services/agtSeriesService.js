3363977cebc9cbf43ee7c9e7bf7d91ec4aff74ea
tocemedo-cpu <tocemedo@gmail.com>
2026-09-15 17:00:16 +0000

    Update agtSeriesService.js

--- backend/src/services/agtSeriesService.js
+++ backend/src/services/agtSeriesService.js
@@ -40,10 +40,20 @@ function construirPedidoSerie({ taxRegistrationNumber, seriesYear, documentType,
     taxRegistrationNumber, seriesYear, documentType, establishmentNumber, seriesContingencyIndicator,
   });
 
+  const dadosAssinatura = {
+  taxRegistrationNumber,
+  seriesYear,
+  documentType,
+  establishmentNumber,
+  seriesContingencyIndicator,
+};
+
+console.log('DADOS ANTES DE ASSINAR:');
+console.log(dadosAssinatura);
   const jwsSignature = agtSigningService.assinarJWS({
     taxRegistrationNumber, seriesYear, documentType, establishmentNumber, seriesContingencyIndicator,
   });
-   logger.info('Software signatura: pedido construído e assinado', jwsSignature);
+  
   const pedido = {
     schemaVersion: '2.0',
     submissionUUID: crypto.randomUUID(),
@@ -64,7 +74,7 @@ function construirPedidoSerie({ taxRegistrationNumber, seriesYear, documentType,
 
   logger.info('Solicitar Série: pedido construído e assinado', pedido);
  
-  return pedido;
+ // return pedido;
 }
 
 /**
