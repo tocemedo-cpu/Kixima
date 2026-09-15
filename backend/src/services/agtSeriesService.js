82eeecad7b6133d1e57aa05e083d022d5a48ff7c
tocemedo-cpu <tocemedo@gmail.com>
2026-09-15 16:37:48 +0000

    Update agtSeriesService.js

--- backend/src/services/agtSeriesService.js
+++ backend/src/services/agtSeriesService.js
@@ -61,8 +61,9 @@ function construirPedidoSerie({ taxRegistrationNumber, seriesYear, documentType,
   // chave privada nunca passa por este log. Ver o pedido completo tal como
   // vai para a AGT ajuda a diagnosticar sem ter de reproduzir o pedido à
   // parte.
+  logger.info('Software signatura: pedido construído e assinado', softwareInfo);
   logger.info('Solicitar Série: pedido construído e assinado', pedido);
-
+ 
   return pedido;
 }
 
